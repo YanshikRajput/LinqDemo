593e8a5f695893732e89285616ccbffb2b9493d6
YanshikRajput <yanshikrajput99@gmail.com>
2021-04-19 12:59:40 +0530

    C# LINQDEMO
    LINQ-DEMO

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<product> list = new List<product>();
+            list.Add(
+                new product
+                {
+                    Id = 100,
+                    Title = "Bat",
+                    Price =  200,
+                    Owner = "Yanshik"
+                });
+            list.Add(new product
+            {
+                Id = 101,
+                Title = "Ball",
+                Price = 200,
+                Owner = "Sachin"
+            });
+            list.Add(new product
+            {
+                Id = 102,
+                Title = "Bails",
+                Price = 500,
+                Owner = "gaurav"
+            });
+            list.Add(new product
+            {
+                Id = 103,
+                Title = "Stumps",
+                Price = 2000,
+                Owner = "Nick"
+            });
+
+            //var Owner = list.Where(p => p.Price > 300).Select(p=>p.Owner).ToList();
+            //Owner.ForEach(a => Console.WriteLine(a));
+           var llist = (from r in list where r.Price > 300 select (r.Owner, r.Id)).ToList();
+            llist.ForEach(a => Console.WriteLine(a));
+        }
+        
+    }
+}
---
+++ product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_assignment
+{
+    public class product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public string Owner { get; set; }
+
+    }
+}
